3597872000125a2eeab37d2ef866f64f7068f6b5
manojsa5855 <101385789+manojsa5855@users.noreply.github.com>
2023-05-30 18:06:10 +0530

    Update vnet.tf

--- modules/vnet.tf
+++ modules/vnet.tf
@@ -1,12 +1,12 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "example-network"
+  name                = "infra-network"
   address_space       = ["12.0.0.0/16"]
   location            =var.location
   resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "az_subnet" {
-  name                 = "public"
+  name                 = "public1"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["12.0.0.0/24"]
